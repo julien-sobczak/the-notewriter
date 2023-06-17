cf2f903a75f73170098832da2831abb2611b6f45
Julien Sobczak <julien.sobczak@gmail.com>
2023-06-17 16:08:33 +0200

    chore: publish to GitHub Pages

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -20,7 +20,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'julien-sobczal', // Usually your GitHub org/user name.
   projectName: 'the-notewriter', // Usually your repo name.
-
+  trailingSlash: false,
+  
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
