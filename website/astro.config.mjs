a7c37344fb3e1e49ea149a1db40a98129e204707
Julien Sobczak <julien.sobczak@gmail.com>
2024-04-11 22:06:54 +0200

    chore: configure GitHub Pages url

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://julien-sobczak.github.io',
+	base: 'the-notewriter',
 	integrations: [
 		starlight({
 			title: 'The NoteWriter Documentation',
